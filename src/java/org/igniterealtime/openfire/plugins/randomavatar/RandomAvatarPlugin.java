202715f554dbb80324eebfa71e1098bf66b9ad8f
Guus der Kinderen <guus.der.kinderen@gmail.com>
2024-11-20 15:30:36 +0100

    fixes #5: Openfire 5.0.0 compatibility
    Updates the plugin to be compatible with Openfire 5.0.0 (in particular: with the new version of Jetty that's part of that release).
    
    This drops compatibility with versions of Openfire prior to 5.0.0.
    
    Drop unnecessary manual initialization of JasperInitializer
    
    To quote Joakim Erdfelt, a Jetty developer:
    
    > For a `WebAppContext`, this manual initialization of JasperInitializer is not necessary. (it is necessary for a `ServletContextHandler`)
    > The fact that jetty-ee8-apache-jsp-<ver>.jar is in your classpath should be enough for that webapp to automatically have JSP support enabled.
    >
    > If you find that you don't have a `jsp` servlet initialized, then that means you'll want to declare the `WebAppContext.setDefaultDescriptor()` to point to the `webdefault-ee8.xml` for your version. You can find that file in the `jetty-ee8-webapp-<ver>-config.jar` -
    > See https://repo1.maven.org/maven2/org/eclipse/jetty/ee8/jetty-ee8-webapp/12.0.14/
    
    Drop explicit InstanceManager configuration
    
    Removing initialization of an attribute that I believe to be something that has been carried over from earlier versions, without this being neccesary anymore.

--- src/java/org/igniterealtime/openfire/plugins/randomavatar/RandomAvatarPlugin.java
+++ src/java/org/igniterealtime/openfire/plugins/randomavatar/RandomAvatarPlugin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2023 Ignite Realtime Foundation. All rights reserved.
+ * Copyright (C) 2019-2024 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,11 +15,7 @@
  */
 package org.igniterealtime.openfire.plugins.randomavatar;
 
-import org.apache.tomcat.InstanceManager;
-import org.apache.tomcat.SimpleInstanceManager;
-import org.eclipse.jetty.apache.jsp.JettyJasperInitializer;
-import org.eclipse.jetty.plus.annotation.ContainerInitializer;
-import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.ee8.webapp.WebAppContext;
 import org.jivesoftware.openfire.container.Plugin;
 import org.jivesoftware.openfire.container.PluginManager;
 import org.jivesoftware.openfire.http.HttpBindManager;
@@ -27,8 +23,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * An Openfire plugin that makes available an avatar-exposing webservice.
@@ -56,10 +50,6 @@ public class RandomAvatarPlugin implements Plugin
         context.setClassLoader( this.getClass().getClassLoader() );
 
         Log.debug( "Ensure the JSP engine is initialized correctly (in order to be able to cope with Tomcat/Jasper precompiled JSPs)." );
-        final List<ContainerInitializer> initializers = new ArrayList<>();
-        initializers.add( new ContainerInitializer( new JettyJasperInitializer(), null ) );
-        context.setAttribute( "org.eclipse.jetty.containerInitializers", initializers );
-        context.setAttribute( InstanceManager.class.getName(), new SimpleInstanceManager() );
         context.setAttribute( "org.igniterealtime.openfire.plugins.randomavatar.plugindirectory", pluginDirectory.toPath() );
         Log.debug( "Registering context with the embedded webserver." );
         HttpBindManager.getInstance().addJettyHandler( context );
